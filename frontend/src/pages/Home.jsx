7c8baac6945a2745d4c1c2a09ec8ae3a49dcca4e
Anil Kandel <kandelanil24@gmail.com>
2025-08-04 23:57:42 -0400

    Updated Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
             <div className="home-content">
                 <div className="text-section">
                     <h1>Hello, I'm Girija Prasad Kandel</h1>
-                    <p>Welcome to my portfolio. I am a passionate web developer who loves building web and mobile applications. Explore my projects and services to learn more about what I do!</p>
+                    <p>Welcome to my personal portfolio. I am a passionate web developer who loves building web and mobile applications. Explore my projects and services to learn more about what I do!</p>
                     <a href="/about" className="btn">Learn More About Me</a>
                 </div>
 
